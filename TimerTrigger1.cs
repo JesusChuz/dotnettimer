dc529576398b272827f9af2df8c2a21fbf55c9c9
JesusChuz <45716413+JesusChuz@users.noreply.github.com>
2023-08-09 10:30:33 -0600

    Update TimerTrigger1.cs

--- TimerTrigger1.cs
+++ TimerTrigger1.cs
@@ -10,7 +10,7 @@ namespace Company.Function
         [FunctionName("TimerTrigger1")]
         public void Run([TimerTrigger("* * * * * *")]TimerInfo myTimer, ILogger log)
         {
-            log.LogInformation($"Test5 C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation($"Test6-s1plan C# Timer trigger function executed at: {DateTime.Now}");
         }
     }
 }
